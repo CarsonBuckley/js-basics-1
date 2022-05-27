5a1bafffcbbbd35a2df2808f647955e606b8ea58
Carson Buckley <45154466+CarsonBuckley@users.noreply.github.com>
2022-05-27 16:55:49 -0600

    Switched testing and implementation

--- sldc.js
+++ sldc.js
@@ -8,8 +8,8 @@
 // Planning
 // Analysis of Requirements
 // Design
-// Testing & Integration
 // Implementation
+// Testing & Integration
 // Maintain
 
 //////////////////PROBLEM 2////////////////////
@@ -20,6 +20,6 @@
 let planning = "Take data from various departments and plan out the basic outline of the project"
 let analysisOfRequirements = "Clearly define what that data has shown you"
 let design = "Layout all of the architectural and structural systems"
-let testingAndIntegration = "Testing out the product to find and fix bugs and to see how the software works and feels for the user"
 let implementation = "Released to the intended market once testing is complete and at least a MVP is ready for launch"
+let testingAndIntegration = "Testing out the product to find and fix bugs and to see how the software works and feels for the user"
 let maintain = "Release updates, add new features, continue to fix bugs that occur"
